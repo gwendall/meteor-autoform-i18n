8d68cfb9fb608daf2b702c7b2d175695eccfcc17
Gwen <gwendall.esnault@gmail.com>
2015-04-20 02:03:39 +0200

    fixed for default values

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "gwendall:autoform-i18n",
   summary: "Internationalization for Autoform",
-  version: "0.1.5",
+  version: "0.1.6",
   git: "https://github.com/gwendall/meteor-autoform-i18n.git"
 });
 
--- shared/lib.js
+++ shared/lib.js
@@ -1,24 +1,32 @@
-SimpleSchema.prototype.i18n = function(namespace) {
+var humanize = function(property) {
+  return property
+  .replace(/_/g, " ")
+  .replace(/(\w+)/g, function(match) {
+    return match.charAt(0).toUpperCase() + match.slice(1);
+  });
+};
+
+SimpleSchema.prototype.i18n = function(jsonPath) {
 
   if (Meteor.isServer) return;
   var schema = this._schema;
   Meteor.startup(function() {
     _.each(schema, function(value, key) {
       if (!value) return;
-      schema[key].label = function() {
-        return TAPi18n.__([namespace, key, "label"].join(".")) || key;
-      }
+      schema[key].label = schema[key].label || function() {
+        return TAPi18n.__([jsonPath, key, "label"].join(".")) || humanize(key);
+      };
       schema[key].autoform = schema[key].autoform || {};
-      schema[key].autoform.placeholder = function() {
-        return TAPi18n.__([namespace, key, "placeholder"].join(".")) || "Type something...";
-      }
-      var options = TAPi18n.__([namespace, key, "options"].join("."), { returnObjectTrees: true });
+      schema[key].autoform.placeholder = schema[key].autoform.placeholder || function() {
+        return TAPi18n.__([jsonPath, key, "placeholder"].join(".")) || "Type something...";
+      };
+      var options = TAPi18n.__([jsonPath, key, "options"].join("."), { returnObjectTrees: true });
       if (!_.isObject(options)) return;
-      schema[key].autoform.options = function() {
-        return TAPi18n.__([namespace, key, "options"].join("."), { returnObjectTrees: true });
+      schema[key].autoform.options = schema[key].autoform.options || function() {
+        return TAPi18n.__([jsonPath, key, "options"].join("."), { returnObjectTrees: true });
       }
-      schema[key].autoform.firstOption = function() {
-        return TAPi18n.__([namespace, key, "placeholder"].join("."));
+      schema[key].autoform.firstOption = schema[key].autoform.firstOption || function() {
+        return TAPi18n.__([jsonPath, key, "placeholder"].join("."));
       }
     });
   });
