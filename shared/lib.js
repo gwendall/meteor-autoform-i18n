4d9017ae827f9c0e5a4d5f9082e1e04c5cd8b7a2
Gwen <gwendall.esnault@gmail.com>
2015-04-22 22:14:38 +0200

    cleanup

--- shared/lib.js
+++ shared/lib.js
@@ -6,29 +6,53 @@ var humanize = function(property) {
   });
 };
 
-SimpleSchema.prototype.i18n = function(jsonPath) {
+var getKeys = function(jsonPath, key) {
+  return TAPi18n.__([jsonPath, key].join("."), { returnObjectTrees: true }) || {};
+}
+
+SimpleSchema.prototype.i18n = function(jsonPath, defaults) {
+
+  defaults = defaults || {};
+  defaults.placeholder = defaults.placeholder || "Type something...";
+  defaults.firstOption = defaults.firstOption || "Select something...";
 
   if (Meteor.isServer) return;
   var schema = this._schema;
   Meteor.startup(function() {
+
     _.each(schema, function(value, key) {
+
       if (!value) return;
-      schema[key].label = schema[key].label || function() {
-        return TAPi18n.__([jsonPath, key, "label"].join(".")) || humanize(key);
-      };
+      var keys = getKeys(jsonPath, key);
+
       schema[key].autoform = schema[key].autoform || {};
-      schema[key].autoform.placeholder = schema[key].autoform.placeholder || function() {
-        return TAPi18n.__([jsonPath, key, "placeholder"].join(".")) || "Type something...";
-      };
-      var options = TAPi18n.__([jsonPath, key, "options"].join("."), { returnObjectTrees: true });
-      if (!_.isObject(options)) return;
-      schema[key].autoform.options = schema[key].autoform.options || function() {
-        return TAPi18n.__([jsonPath, key, "options"].join("."), { returnObjectTrees: true });
+
+      if (schema[key].autoform.placeholder || keys.placeholder) {
+        schema[key].autoform.placeholder = schema[key].autoform.placeholder || function() {
+          return getKeys(jsonPath, key).placeholder || defaults.placeholder;
+        };
       }
-      schema[key].autoform.firstOption = schema[key].autoform.firstOption || function() {
-        return TAPi18n.__([jsonPath, key, "placeholder"].join(".")) || "Select something...";
+
+      if (schema[key].autoform.options || keys.options) {
+        schema[key].autoform.options = schema[key].autoform.options || function() {
+          return getKeys(jsonPath, key).options;
+        }
       }
+
+      if (schema[key].autoform.firstOption || keys.options) {
+        schema[key].autoform.firstOption = schema[key].autoform.firstOption || function() {
+          return getKeys(jsonPath, key).placeholder || defaults.firstOption;
+        }
+      }
+
+      if (schema[key].label || keys.label) {
+        schema[key].label = schema[key].label || function() {
+          return getKeys(jsonPath, key).label || humanize(key);
+        };
+      }
+
     });
+
   });
 
 }
