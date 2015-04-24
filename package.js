b9d45c80b20ea7de34bd67abaf653b4f02f6a156
Gwen <gwendall.esnault@gmail.com>
2015-04-24 15:06:41 +0200

    remove log

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "gwendall:autoform-i18n",
   summary: "Internationalization for Autoform",
-  version: "0.1.8",
+  version: "0.1.9",
   git: "https://github.com/gwendall/meteor-autoform-i18n.git"
 });
 
--- shared/lib.js
+++ shared/lib.js
@@ -27,28 +27,24 @@ SimpleSchema.prototype.i18n = function(jsonPath, defaults) {
     schema[key].autoform = schema[key].autoform || {};
 
     if (schema[key].autoform.placeholder || keys.placeholder) {
-      console.log("Setting placeholder.", key);
       schema[key].autoform.placeholder = schema[key].autoform.placeholder || function() {
         return getKeys(jsonPath, key).placeholder || defaults.placeholder;
       };
     }
 
     if (schema[key].autoform.options || keys.options) {
-      console.log("Setting options.", key);
       schema[key].autoform.options = schema[key].autoform.options || function() {
         return getKeys(jsonPath, key).options;
       }
     }
 
     if (schema[key].autoform.firstOption || keys.options) {
-      console.log("Setting first option.", key);
       schema[key].autoform.firstOption = schema[key].autoform.firstOption || function() {
         return getKeys(jsonPath, key).placeholder || defaults.firstOption;
       }
     }
 
     if (schema[key].label || keys.label) {
-      console.log("Setting label.", key);
       schema[key].label = schema[key].label || function() {
         return getKeys(jsonPath, key).label || humanize(key);
       };
